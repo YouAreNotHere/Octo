8b71ce2caae3e8fce275502b17729502395acb39
YouAreNotHere <malferety@gmail.com>
2025-02-07 00:05:46 +0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+      <>
+        <body>
+          <header className='header'>
+              <h1 className='header-title'>
+                  Solutions for people like you
+              </h1>
+          </header>
+        <section className='first-article'>
+            <h2 className='section-name'>
+                Category | Marketing
+            </h2>
+        </section>
+        </body>
+      </>
+  )
+}
+
+export default App
