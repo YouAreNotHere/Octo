d1cd3ad7cd4dd13849993c406ca04ab06717b1d6
YouAreNotHere <malferety@gmail.com>
2025-02-08 22:21:54 +0500

    Сверстал версию для desktop

--- src/features/advertisement/Advertisement.tsx
+++ src/features/advertisement/Advertisement.tsx
@@ -4,19 +4,21 @@ const Advertisement = () => {
     return(
         <div className="advertisement-wrapper">
             <section className="advertisement-section">
-                <h1 className='advertisement-header'>
-                    Solutions for people like you
-                </h1>
-                <p className='advertisement-body'>
-                    How can we help your technology and services business develop a revenue engine based on Hubspot?
-                </p>
-                <div className="buttons-wrapper">
-                    <button className='first-advertisement-button'>
-                        Call to action
-                    </button>
-                    <button className='second-advertisement-button'>
-                        Call to action
-                    </button>
+                <div className="text-and-buttons-wrapper">
+                    <h1 className='advertisement-header'>
+                        Solutions for people like you
+                    </h1>
+                    <p className='advertisement-body'>
+                        How can we help your technology and services business develop a revenue engine based on Hubspot?
+                    </p>
+                    <div className="buttons-wrapper">
+                        <button className='first-advertisement-button'>
+                            Call to action
+                        </button>
+                        <button className='second-advertisement-button'>
+                            Call to action
+                        </button>
+                    </div>
                 </div>
                 <img
                     className='advertisement-img'
